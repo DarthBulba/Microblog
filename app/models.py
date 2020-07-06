7fcec94012e8aba1572ba5d842463c70f52395ba
Dmitry Ganin <dg@bamboogroup.eu>
2020-07-07 00:48:18 +0300

    update login, add logout, update base.html

--- app/models.py
+++ app/models.py
@@ -3,7 +3,12 @@ from datetime import datetime
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from app import db
+from app import db, login
+
+
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class User(db.Model, UserMixin):
