08910ef4885e076442d9d20d46371ea78eb8ea5f
Dmitry Ganin <dg@bamboogroup.eu>
2020-07-09 01:32:39 +0300

    add 'edit profile' feature

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
 
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
 
 
@@ -60,6 +60,7 @@ def register():
         return redirect(url_for('index'))
     form = RegistrationForm()
     if form.validate_on_submit():
+        # noinspection PyArgumentList
         u = User(username=form.username.data, email=form.email.data)
         u.set_password(form.password.data)
         db.session.add(u)
@@ -78,6 +79,22 @@ def user(username):
         {'author': u, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=u, posts=posts)
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
 
 
 @app.before_request
